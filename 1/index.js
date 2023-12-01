895a8fafe686a33b7c90b8d171b78bc217b0f104
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-01 10:54:50 -0600

    added tsx to run ts files automatically

--- 1/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Day 1: 
- */
-
-import { input } from "./input.js";
-console.log('this is our input', input);
---
+++ 1/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Day 1: 
+ */
+
+import { input } from "./input.ts";
+console.log('this is our input', input);
+
+// Step 1: Find the first integer in the string
+
+/**
+ * 
+ * @param {Array} input an array of strings
+ */
+const getInputValue = (input) => {
+    console.log('Does our input look correct?', input);
+}
+
+getInputValue(input);
+
+// Step 2: Find the last integer in the string
+
+// Step 3: Sum the first and last integer to get a value for the string
+
+// Step 4: iterate through the remaining ones to get their values as well
+
+// Step 5: Add all of the integers together
+
+
