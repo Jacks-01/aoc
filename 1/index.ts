7dc9989199ba65bc3e9f9b994a81e44dbcb532f0
Jack Stubblefield <jack011601@gmail.com>
2023-12-07 11:53:11 -0600

    function not returning the right values

--- 1/index.ts
+++ 1/index.ts
@@ -22,10 +22,13 @@ const getInputValue = (input: Array<string>): number => {
 
 	input.forEach((inputString) => {
 		let firstNumber = getFirstNumber(inputString);
-        let lastNumber = getLastNumber(inputString);
-        let combinedValue = parseInt((firstNumber + lastNumber));
+		let lastNumber = getLastNumber(inputString);
+        let combinedValue = firstNumber + lastNumber;
+        console.log('what is this?:', combinedValue)
 
-        totalSum += combinedValue;
+        let sum = parseInt(combinedValue)
+        totalSum += sum
+        console.log('???', typeof(parseInt(combinedValue)))
 	});
 
 	// console.log(totalSum);
@@ -34,25 +37,25 @@ const getInputValue = (input: Array<string>): number => {
 
 const getFirstNumber = (input: string): string => {
 	// console.log('getFirstNumber input:', input);
-    let firstNumber: string = '';
-    // console.log('getFirstNumber() input:', input)
+	let firstNumber: string = '';
+	// console.log('getFirstNumber() input:', input)
 
 	for (let i = 0; i < input.length; i++) {
-        let element: string = input.charAt(i);
-        // console.log('element:', element)
+		let element: string = input.charAt(i);
+		// console.log('element:', element)
 
-        if (parseInt(element)) {
+		if (parseInt(element)) {
 			firstNumber = element;
 			// console.log('number', firstNumber);
 			return firstNumber;
 		} else {
-            // console.log('letter');
-            i++;
-        }
-        i++;
+			// console.log('letter');
+			i++;
+		}
+		i++;
 	}
 
-	if (firstNumber.length) {
+	if (firstNumber.length === 0) {
 		console.log('no number in the input!');
 		return '';
 	}
@@ -71,11 +74,11 @@ const getLastNumber = (input: string): string => {
 			return lastNumber;
 		} else {
 			i++;
-        }
-        i++;
+		}
+		i++;
 	}
 
-	if (!lastNumber) {
+	if (lastNumber.length === 0) {
 		console.log('no number in the input!');
 		return '';
 	}
@@ -88,8 +91,6 @@ const reverseString = (input: string): string => {
 	return reversedString;
 };
 
-// getFirstNumber(testInput[1])
-console.log(getLastNumber(testInput[1]))
 // console.log('t1', getFirstNumber(testInput[0]));
 // console.log('t1', getLastNumber(testInput[0]));
 // console.log('t2', getFirstNumber(testInput[1]));
@@ -99,4 +100,5 @@ console.log(getLastNumber(testInput[1]))
 // console.log('t4', getFirstNumber(testInput[3]));
 // console.log('t4', getLastNumber(testInput[3]));
 
+console.log(getInputValue(testInput));
 console.timeEnd('aoc1');
