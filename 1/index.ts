1a79500ecbeef1851adb40fc2a888ea64349c1d5
Jack Stubblefield <jack011601@gmail.com>
2023-12-07 10:12:22 -0600

    getting first number from input works

--- 1/index.ts
+++ 1/index.ts
@@ -1,28 +1,43 @@
 /**
- * Day 1: 
+ * Day 1:
  */
 
-import { input } from "./input.ts";
+import { input } from './input.ts';
 console.log('this is our input', input);
 
 // Step 1: Find the first integer in the string
+//! Can create a function that starts at the beginning and finds the first number
+//! cn create another function that reverses the string and finds the last number
+//! Add the two numbers and move to the next value
 
 /**
- * 
+ *
  * @param {Array} input an array of strings
  */
-const getInputValue = (input) => {
-    console.log('Does our input look correct?', input);
-}
+const getInputValue = (input: Array<string>): number => {
+	console.log('Does our input look correct?', input);
+	let totalSum: number = 0;
+	return totalSum;
+};
 
 getInputValue(input);
 
-// Step 2: Find the last integer in the string
+const getFirstNumber = (input: string): number => {
+	console.log('getFirstNumber input:', input);
+	let firstNumber: number = 0;
 
-// Step 3: Sum the first and last integer to get a value for the string
+	for (let i = 0; i < input.length; i++) {
+		const element: string = input.charAt(i);
 
-// Step 4: iterate through the remaining ones to get their values as well
+		if (parseInt(element)) {
+			firstNumber += parseInt(element);
+			console.log(firstNumber);
+            return firstNumber;
+        }
+        else {
+            i++;
+        }
+	}
+};
 
-// Step 5: Add all of the integers together
-
-
+getFirstNumber('twoknbxlczgd5');
