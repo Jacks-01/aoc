dea52e8b2ce76fdc896edc1943b3688ad57ce948
Jack Stubblefield <jack011601@gmail.com>
2023-12-08 14:11:46 -0600

    changed function to read file input instead of test array

--- 2/index.ts
+++ 2/index.ts
@@ -1,10 +1,3 @@
-const testInput: string[] = [
-	'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-	'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-	'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-	'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-	'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
-];
 import fs from 'fs';
 
 const colorTotals: Object = {
@@ -13,24 +6,34 @@ const colorTotals: Object = {
 	blue: 14
 };
 
-const checkGames = (input): any => {
-	// const lines = fs.readFileSync(file, 'utf-8').trim().split('\n'); // read the file
+const testInput: string[] = [
+	'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+	'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+	'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+	'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+	'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+];
 
-	let output: string[] = input.map((game) => {
+const checkGames = (input): any => {
+	const lines: string[] = fs.readFileSync(input, 'utf-8').trim().split('\r'); // read the file
+    console.log(lines)
+	let output: any[] = lines.map((game) => {
 		let gamesArray = game.split(': ')[1].split('; '); //* First, lets split the games into their rounds.
 		let roundsArray = gamesArray.map((round) => {
 			let results: string[] = round.split(', '); //* Now we have each round in a game parsed into an array
 			return results.every((result) => {
 				const [count, color] = result.split(' '); // split each color and value
-                return colorTotals[color] >= count; // compare them to our colorTotals object.
-            });
+				return colorTotals[color] >= count; // compare them to our colorTotals object.
+			});
 		});
 
-        
 		return roundsArray.every((p) => p);
-    });
-    
+	});
+
 	console.log('this is our output', output);
+	return output.reduce((sum, result, i) => {
+		return result ? sum + (i + 1) : sum;
+	}, 0);
 };
 
-console.log(checkGames(testInput));
+console.log(checkGames('input.ts'));
